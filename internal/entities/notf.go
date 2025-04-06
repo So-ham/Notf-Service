4458d7475d6997e913a97b30e1e32f6b413f9e69
Soham Das <soham.das@converj.com>
2025-04-06 22:20:24 +0530

    idx update

--- internal/entities/notf.go
+++ internal/entities/notf.go
@@ -8,7 +8,7 @@ type FlaggedNotf struct {
 	ID         uint      `json:"id" gorm:"primaryKey"`
 	CreatedAt  time.Time `json:"created_at"`
 	UpdatedAt  time.Time `json:"updated_at"`
-	UserID     uint      `json:"user_id"`
+	UserID     uint      `json:"user_id" gorm:"index"`
 	Severity   string    `json:"severity"`
 	Content    string    `json:"content"`
 	NotifiedAt time.Time `json:"notified_at,omitempty"`
